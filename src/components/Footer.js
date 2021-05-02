33b74e5b621c76207c0b798fe45513f643d3920a
Luke Lorenger <dirtydeadtv@gmail.com>
2021-05-02 18:59:03 -0400

    footer chores

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import { Button } from './Button';
 import './Footer.css';
 
@@ -21,10 +22,49 @@ function Footer() {
                 <div className="footer-link-wrapper">
                     <div className="footer-link-items">
                         <h2>About Us</h2>
-                        
                     </div>
                 </div>
             </div>
+            <section className="social-media">
+                <div className="social-media-wrap">
+                    <div className="footer-logo">
+                        <Link to='/' className="social-logo">
+                            TRVL <i className="fab fa-typo3"></i>
+                        </Link>
+                    </div>
+                    <small className='website-rights'>TRVL © 2021</small>
+                    <div className="social-icons">
+                        <Link className="social-icon-link facebook"
+                         to="/" 
+                         target="_blank"
+                         aria-label='Facebook'
+                         >
+                        <i className="fab fa-facebook-f"></i>
+                        </Link>
+                        <Link className="social-icon-link instagram"
+                         to="/" 
+                         target="_blank"
+                         aria-label='Instagram'
+                         >
+                        <i className="fab fa-instagram"></i>
+                        </Link>
+                        <Link className="social-icon-link linkedin"
+                         to="/" 
+                         target="_blank"
+                         aria-label='LinkedIn'
+                         >
+                        <i className="fab fa-linkedin"></i>
+                        </Link>
+                        <Link className="social-icon-link twitter"
+                         to="/" 
+                         target="_blank"
+                         aria-label='Twitter'
+                         >
+                        <i className="fab fa-twitter"></i>
+                        </Link>
+                    </div>
+                </div>
+            </section>
         </div>
     );
 }
